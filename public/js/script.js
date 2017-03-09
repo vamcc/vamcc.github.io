0ad5c9356d7afb1be98b78ea2692a0fb6c30d604
yechao <yechao@qiyi.com>
2017-03-09 19:02:13 +0800

    加个背景变换

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,22 @@
-document.querySelector('.header').addEventListener('mouseenter', function() {
-    alert('hahaha')
+document.querySelector('#cantWait').addEventListener('click', function() {
+    let word = document.querySelector('.word .w2')
+    let body = document.querySelector('body')
+    document.querySelector('.w1').classList.add('move')
+    document.querySelector('.w2').classList.add('move')
+    this.remove()
+    setInterval(() => {
+        let obj = getRandomColor()
+        body.style.backgroundColor = obj.body
+        word.style.color = obj.word
+    }, 2e3)
 })
+
+function getRandomColor() {
+    let rColor = () => Math.floor(Math.random() * 255)
+    let bodyCls = [rColor(), rColor(), rColor()]
+    let wordCls = [255 - bodyCls[0], 255 - bodyCls[1], 255 - bodyCls[2]]
+    return {
+        body: `rgb(${bodyCls[0]}, ${bodyCls[1]}, ${bodyCls[2]})`,
+        word: `rgb(${wordCls[0]}, ${wordCls[1]}, ${wordCls[2]})`,
+    }
+}
